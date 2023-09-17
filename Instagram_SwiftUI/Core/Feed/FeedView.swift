e5435da935daa73d413fad1f1894d172b0f5a5d0
Rocky <rdev99@naver.com>
2023-09-17 17:04:47 +0900

    [FEAT] Feed View

---
+++ Instagram_SwiftUI/Core/Feed/FeedView.swift
@@ -0,0 +1,43 @@
+//
+//  FeedView.swift
+//  Instagram_SwiftUI
+//
+//  Created by Rocky on 2023/09/17.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { post in
+                        FeedCell()
+                    }
+                }
+                .padding(.top, 8)
+            }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image("instagram_logo_black")
+                        .resizable()
+                        .frame(width: 100, height: 28)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+            }
+        }
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
