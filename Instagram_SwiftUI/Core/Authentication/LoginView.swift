9ba727a9ce2ba4c760b3625c00f4b3b132f28542
Rocky <rdev99@naver.com>
2023-09-17 18:20:26 +0900

    [FEAT] Login View

---
+++ Instagram_SwiftUI/Core/Authentication/LoginView.swift
@@ -0,0 +1,119 @@
+//
+//  LoginView.swift
+//  Instagram_SwiftUI
+//
+//  Created by Rocky on 2023/09/17.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+                Spacer()
+                
+                // logo image
+                Image("instagram_logo_black")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 220, height: 90)
+                
+                // text fields
+                VStack {
+                    TextField("Enter your email", text: self.$email)
+                        .textInputAutocapitalization(.never) // 첫 글자를 자동으로 대문자로 설정하는 기능 비활성화
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Enter your password", text: self.$password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                }
+                
+                Button {
+                    print("Show forgot password")
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                Button {
+                    print("Login")
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 360, height: 44)
+                        .background(Color(.systemBlue))
+                        .cornerRadius(10)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Rectangle()
+                        .frame(width: UIScreen.main.bounds.width / 2 - 40, height: 0.5)
+                    
+                    Text("OR")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    
+                    Rectangle()
+                        .frame(width: UIScreen.main.bounds.width / 2 - 40, height: 0.5)
+                }
+                .foregroundColor(.gray)
+                
+                HStack(spacing: 2) {
+                    Image("facebook_logo")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 27, height: 27)
+                    
+                    Text("Continue with Facebook")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(.systemBlue))
+                }
+                .padding(.top, 8)
+                
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink {
+                    Text("Sign up")
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                }
+                .padding(.vertical, 16)
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
