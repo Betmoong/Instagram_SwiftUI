928e28982d7f0f0da1b13e0470bf3c5660c89418
iOSRocky <lim.rocky@cashwalk.io>
2023-09-17 14:25:43 +0900

    Project Setup

---
+++ Instagram_SwiftUI/App/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_SwiftUIApp.swift
+//  Instagram
+//
+//  Created by Rocky on 2023/09/17.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
