c98ae9d7dd7dd6cf54a905509b6006b3c4a60e9a
Lim Rocky <lim.rocky@cashwalk.io>
2023-09-18 16:53:44 +0900

    [FEAT] User Profile Navigation

---
+++ Instagram_SwiftUI/Core/Profile/CurrentUserProfileView.swift
@@ -0,0 +1,103 @@
+//
+//  CurrentUserProfileView.swift
+//  Instagram_SwiftUI
+//
+//  Created by Rocky on 2023/09/18.
+//
+
+import SwiftUI
+
+struct CurrentUserProfileView: View {
+    private let gridItems: [GridItem] = [
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1)
+    ]
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                // header
+                VStack(spacing: 10) {
+                    // picture and stats
+                    HStack {
+                        Image("TomHardy")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                        
+                        Spacer()
+                        
+                        HStack(spacing: 8) {
+                            UserStatView(value: 3, title: "Posts")
+                            
+                            UserStatView(value: 12, title: "Followers")
+                            
+                            UserStatView(value: 24, title: "Following")
+                        }
+                    }
+                    .padding(.horizontal)
+                    
+                    // name and bio
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Chadwick Bozeman")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Text("Wakanda Forever")
+                            .font(.footnote)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    
+                    // action button
+                    Button {
+                        
+                    } label: {
+                        Text("Edit Profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 360, height: 32)
+                            .foregroundColor(.black)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 6)
+                                    .stroke(.gray, lineWidth: 1)
+                            )
+                    }
+                    
+                    Divider()
+                }
+                
+                // post grid view
+                
+                LazyVGrid(columns: self.gridItems, spacing: 1) {
+                    ForEach(0 ... 15, id: \.self) { index in
+                        Image("TomHardy")
+                            .resizable()
+                            .scaledToFill()
+                    }
+                }
+            }
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        print("DEBUG: Click")
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                            .foregroundColor(.black)
+                    }
+                    
+                }
+            }
+        }
+    }
+}
+
+struct CurrentUserProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrentUserProfileView()
+    }
+}
