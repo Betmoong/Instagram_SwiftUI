41eaeb1074042dac0981b58bedc63dd1c39f175e
Rocky <rdev99@naver.com>
2023-09-23 19:13:30 +0900

    [FEAT] Upload User Data to Cloud Firestore

--- Instagram_SwiftUI/Core/Authentication/Service/AuthService.swift
+++ Instagram_SwiftUI/Core/Authentication/Service/AuthService.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestoreSwift
+import Firebase
 
 class AuthService {
     
@@ -33,6 +35,9 @@ class AuthService {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
+            print("DEBUG: Did create user..")
+            await self.uploadUserData(uid: result.user.uid, username: username, email: email)
+            print("DEBUG: Did upload user data..")
         } catch {
             print("DEBUG: Failed to register user with error \(error.localizedDescription)")
         }
@@ -45,5 +50,12 @@ class AuthService {
     func signout() {
         try? Auth.auth().signOut()
         self.userSession = nil
+    }
+    
+    // 데이터를 업로드할 땐 일반적으로 throw를 사용하지 않는다. 왜냐하면 데이터가 잘못되는 일이 거의 없다(?)
+    private func uploadUserData(uid: String, username: String, email: String) async {
+        let user = User(id: uid, username: username, email: email)
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
+        try? await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
     }
 }
