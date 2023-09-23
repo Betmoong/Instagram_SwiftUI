efb34fa6dff8ebddc0d29fa12aa5caa5e8483f14
Rocky <rdev99@naver.com>
2023-09-23 16:43:43 +0900

    [FEAT] Authentication Service

---
+++ Instagram_SwiftUI/Core/Root/ViewModel/ContentViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ContentViewModel.swift
+//  Instagram_SwiftUI
+//
+//  Created by Rocky on 2023/09/23.
+//
+
+import Foundation
+import Firebase
+import Combine
+
+class ContentViewModel: ObservableObject {
+    
+    private let service = AuthService.shared
+    private var cancellables = Set<AnyCancellable>()
+    
+    @Published var userSession: FirebaseAuth.User?
+    
+    init() {
+        self.setupSubscribers()
+    }
+    
+    func setupSubscribers() {
+        service.$userSession.sink { [weak self] userSession in
+            self?.userSession = userSession
+        }
+        .store(in: &cancellables)
+    }
+}
